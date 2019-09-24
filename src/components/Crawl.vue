dddcd463eab6d8857b64840164e306144ccb13e5
scottdspiller <Scott.Spiller@flyfrontier.com>
2019-09-24 15:00:37 -0600

    work.

--- src/components/Crawl.vue
+++ src/components/Crawl.vue
@@ -4,15 +4,10 @@
             <section class="star-wars">
             <div class="crawl">
                 <div class="title">
-                <p>Episode IV</p>
-                <h1>A New Hope</h1>
+                <h1>{{SELECTEDPERSON.name}}</h1>
                 </div>
                 
-                <p>It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire.</p>
-                
-                <p>During the battle, Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet.</p>
-
-                <p>Pursued by the Empire’s sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy….</p>
+                <p v-if="SELECTEDPERSON.gender !== 'n/a'">{{SELECTEDPERSON.gender}}</p>
             </div>
             </section>
     </div>
@@ -21,11 +16,14 @@
 <script lang="ts">
 import Vue from 'vue';
 import {Person} from '../models/Person';
+import { mapGetters } from 'vuex'
 
 export default Vue.extend({
   name: 'Crawl',
   computed:{
-      
+      ...mapGetters([
+      'SELECTEDPERSON'
+      ])
   }
   
 });
