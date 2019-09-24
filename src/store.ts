2d4a18a4e5a09dbd6d425e0eef3817df0b8864f9
scottdspiller <Scott.Spiller@flyfrontier.com>
2019-09-24 15:00:27 -0600

    Work.

--- src/store.ts
+++ src/store.ts
@@ -2,21 +2,27 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import Axios from 'axios';
 import {People} from './models/People';
+import { Person } from './models/Person';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     people : new People(),
+    selectedPerson : new Person(),
   },
   getters : {
     PEOPLE : (state) => {
       return state.people;
     },
+    SELECTEDPERSON : (state) => {
+      return state.selectedPerson;
+    },
   },
   mutations: {
     SET_PEOPLE : (state,payload) => {
       state.people = new People(payload);
+      state.selectedPerson = new Person(state.people.persons[0]);
     },
   },
   actions: {
