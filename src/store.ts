8956ce359e453f20e5f40249db9379bad7e39a2d
scottdspiller <Scott.Spiller@flyfrontier.com>
2019-09-24 14:24:34 -0600

    Get People from vuex store

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,28 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import Axios from 'axios';
+import {People} from './models/People';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    people : new People(),
+  },
+  getters : {
+    PEOPLE : (state) => {
+      return state.people;
+    },
   },
   mutations: {
-
+    SET_PEOPLE : (state,payload) => {
+      state.people = new People(payload);
+    },
   },
   actions: {
-
+    GET_PEOPLE : async (context,payload) => {
+      let { data } = await Axios.get('https://swapi.co/api/people/?format=json');
+      context.commit('SET_PEOPLE',data);
+   },
   },
 });
